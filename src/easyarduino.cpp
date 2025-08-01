c24515a818d43b586515c464d878d53d87cbfc8b
TheProgrammerFundation <jatega11@hotmail.com>
2025-08-01 10:33:26 +0200

    Update easyarduino.cpp

--- src/easyarduino.cpp
+++ src/easyarduino.cpp
@@ -4,6 +4,32 @@
 EasyButton::EasyButton() {
   // Constructor vacío, no hace nada
 }
+Led::Led(){
+
+}
+
+void Led::begin(uint8_t pin){
+  _pin = pin;
+  pinMode(_pin, OUTPUT);//Configura el led como output
+}
+void Led::pulse(){
+   for (int i = 0; i <= 255; i++) {
+    analogWrite(_pin, i);   // Incrementa el brillo
+    delay(10);
+  }
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(_pin, i);   // Disminuye el brillo
+    delay(10);
+  }
+}
+
+void Led::on(){
+   digitalWrite(_pin,HIGH);
+   isOn = TRUE
+}
+
+void Led::off(){
+   digitalWrite
 
 void EasyButton::begin(uint8_t pin) {
   _pin = pin;
