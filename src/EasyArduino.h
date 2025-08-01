e80b425b77a9f69783d939ec89792157dcbb660c
TheProgrammerFundation <jatega11@hotmail.com>
2025-08-01 10:14:26 +0200

    Updated EasyArduino.h

--- src/EasyArduino.h
+++ src/EasyArduino.h
@@ -10,10 +10,19 @@ class EasyButton {
     EasyButton();
     void begin(uint8_t pin);
     bool isPressed();
-  
 
   private:uint8_t _pin;
 
 };
+class Led {
+  public:
+    Led();
+    void begin(uint8_t pin);
+    bool isOn();
+    void toggle();
+    void on();
+    void off();  
+  private:uint8_t _pin;
 
+};
 #endif
