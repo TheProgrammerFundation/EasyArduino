0b77f501f16a7eddcd6fb167692383ff69477147
TheProgrammerFundation <jatega11@hotmail.com>
2025-08-01 10:33:13 +0200

    Update EasyArduino.h

--- src/EasyArduino.h
+++ src/EasyArduino.h
@@ -1,7 +1,5 @@
 #ifndef EASY_ARDUINO_H
-
 #define EASY_ARDUINO_H
-
 
 #include <Arduino.h>
 
@@ -11,19 +9,24 @@ class EasyButton {
     void begin(uint8_t pin);
     bool isPressed();
 
-  private:uint8_t _pin;
-
+  private:
+    uint8_t _pin;
 };
+
 class Led {
   public:
     Led();
     void begin(uint8_t pin);
-    bool isOn();
-    void toggle();
+    bool isOn();        // Devuelve true si el LED está encendido
+    void toggle();      // Cambia el estado del LED
     void on();
     void off();
     void pulse();
-  private:uint8_t _pin;
 
+  private:
+    uint8_t _pin;
+    bool _isOn;         // Estado interno del LED
 };
+
 #endif
+
